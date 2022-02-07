1d9fad864024776854898913dee1b228042437db
Nouira Alim <nouiraalim@gmail.com>
2022-02-07 10:04:34 +0100

    Tests avec printf, sprintf et vprintf

--- index.php
+++ index.php
@@ -6,9 +6,34 @@ echo chr(101); // Affiche caractère depuis code ASCII
 $version = 8;
 $langue = "fr";
 $url = "PHP %d est disponible sur l'adresse https://%s.php.net";
+printf($url, $version, $langue);
+
+$reponse = sprintf($url, $version, $langue);
+echo $reponse;
+
 printf("<br />");
 printf("%b <br />", 3);
 printf("%o %x <br />", 15, 20);
 printf("%03d <br />", 2); // Affiche 002
 printf("%03d <br />", 20); //Afficge 020
-printf("%-4s Michel<br />", "Bonjour");
+printf("[%-10.9s]\n", "Bonjour");
+
+$fr = "%d est la version de %s";
+$en = "%d is %s's version";
+
+$langue = $fr;
+
+if ($langue === "en") 
+{
+    printf($en, 8.1, "PHP");
+} 
+else 
+{
+    printf($fr, 8.1, "PHP");
+}
+
+echo "<hr />";
+
+// Affiche une chaîne depuis une entrée sous forme de tableau
+$infos = ["PHP", 8.1];
+vprintf("%s %d", $infos);
